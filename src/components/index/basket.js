40e7b4ede67f15df25125e74d00a8014f160629c
sergeykotenko <sergeykotenko@bk.ru>
2020-12-03 04:13:10 +0300

    class add

--- src/components/index/basket.js
+++ src/components/index/basket.js
@@ -1,141 +1,140 @@
-    class Query {
-        constructor(query) {
-            this.query = query
-            this.init()
-        }
-        init(){
-          return document.querySelector(this.query)
-        }
+let itemss = []
+
+class Query {
+    constructor(query) {
+        this.query = query
+        // this.url = url       
     }
-
-    let containerClass = new Query('#basket-items')
-    let container = containerClass.init()
-    let wrapperClass = new Query('#basket-inner')
-    let wrapper = wrapperClass.init()
-    let totalContainerClass =  new Query('#basket-sum')
-    let totalContainer = totalContainerClass.init()
-
-    const basket = {
-        items: [], // массив с товара и ценами
-        url: 'https://raw.githubusercontent.com/sergeykotenkogithub/imageProject/main/json/basket.json',
-        sum: 0,
-        // container: null, // basket-items (В DOM <div> с товарами и ценами)
-        // wrapper: null, //basket all
-         // 
-        // totalContainer: null,
-        // Инициализация. Основное
-        init() { 
-            
-            // this.container = document.querySelector('#basket-items');            
-            // this.wrapper = document.querySelector('#basket-inner');
-            // this.totalContainer = document.querySelector('#basket-sum');
-
-           //async
-          this._get(this.url) //Метод подключения к json на git
-          .then(basket => { // название basket не влияет
-            
-            
-            this.items = basket.content; // content из url файла
-            // itemss = basket.content
-            
-            
-            
-            this._render();
-            this._handleEvents();
-          });
-            // this._handleEvents2()
-        },
-
-        _get(url) {
-            return fetch(url).then(d => d.json()) // сделает запрос за джейсоном, дождётся ответа и преобразует json в объект, который вернётся из даного метода
-        },
-    
-        _render() {
-            let htmlStr = '';
-
-            this.items.forEach((item, i) => {
-                htmlStr += this.renderBasketTemplate(item, i);
-            });
-            container.innerHTML = htmlStr;
-            this._calcSum();
-        },
-
-        // Подсчёт стоимости общей
-        _calcSum() {
-            this.sum = 0;
-            this.items.forEach(item => {
-                this.sum += item.amount * item.productPrice;
-            });
-
-            totalContainer.innerText = this.sum;
-        },
-
-        // В item мы пробрасываем объект, содержащий данные в том числе и id
-        // item мы находим через catalog.js
-        add(item) {      
-           let find = this.items.find(el => item.productId == el.productId);
-
-           if(find) {
-               find.amount++
-           } else {
-               this.items.push(Object.assign({}, item, {amount: 1}));
-           }
-
-           this._render();
-        },
-        // 
-        _remove(id) {            
-            let find = this.items.find(el => el.productId == id);
-
-            if(find.amount > 1) {
-                find.amount--;
-            } else {
-                this.items.splice(this.items.indexOf(find), 1) // 1 - значит 1 элемент
-            }
-
-            this._render();
-        },
-        _handleEvents() {          
-           document.querySelector('#basket-btn').addEventListener('click', e => {
-            wrapper.classList.toggle('hidden')
-            // toggle убирает класс, а если есть то добовляет, вот и получается что при нажатиее он показывается, а при втором закрывается (hidden в _header.scss)
-           });
-           
-            //Удаление
-            container.addEventListener('click', event => {
-            if(event.target.name == 'remove') {
-                this._remove(event.target.dataset.id)
-            }
-        });
-        },
-
-        renderBasketTemplate(item, i) {
-            return `
-            <div class="cartFlex">
-                <div><img   src="${item.productImg}" alt="buy4"></div>
+    // fdd() { 
+    //     urlCatalog.then(catalog => {
+    //     itemss = catalog; // тут items был массивом (можно даже ему null задать, ничего не поменяется) и стал подтягивать массив из url
+    //     catalog._render();
+    //     catalog._handleEvents();
+    //   });
+    // }
+    init(){
+    return document.querySelector(this.query)
+    }
+    // Метод подключения к json на git
+    get(){
+    return fetch(this.query).then(d => d.json()) 
+    }
+    render(){
         
-                <div class="textCenterCart">
-                    <div class="textByCart">${item.productName}</div>
-                <div>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                </div>                  
-                <div class="priceCart">
-                    ${item.amount} x <span>${item.productPrice}</span> = ${item.amount * item.productPrice}
-                </div>
-                </div>
-                <div class="cartCircle">
-                    <a href="#" class="far fa-times-circle faCart" name="remove" data-id="${item.productId}"></a>
-                </div>        
-            </div> 
-            <div class="horizontal cartHorizontal"></div>   
-        `
-        }
     }
-    basket.init();
+ } 
+
+let containerClass = new Query('#basket-items')
+let container = containerClass.init()
+let wrapperClass = new Query('#basket-inner')
+let wrapper = wrapperClass.init()
+let totalContainerClass =  new Query('#basket-sum')
+let totalContainer = totalContainerClass.init()
+
+let urlClass = new Query('https://raw.githubusercontent.com/sergeykotenkogithub/imageProject/main/json/basket.json')
+let url = urlClass.get()
+
+
+const basket = {
+    items: [], // массив с товара и ценами
+    sum: 0,
+
+    // Инициализация. Основное
+    init() { 
+
+        //async 
+    url.then(basket => { // название basket не влияет  
+        this.items = basket.content; // content из url файла
+        this._render();
+        this._handleEvents();
+        });
+    },
+    _render() {
+        let htmlStr = '';
+
+        this.items.forEach((item, i) => {
+            htmlStr += this.renderBasketTemplate(item, i);
+        });
+        container.innerHTML = htmlStr;
+        this._calcSum();
+    },
+
+    // Подсчёт стоимости общей
+    _calcSum() {
+        this.sum = 0;
+        this.items.forEach(item => {
+            this.sum += item.amount * item.productPrice;
+        });
+
+        totalContainer.innerText = this.sum;
+    },
+
+    // В item мы пробрасываем объект, содержащий данные в том числе и id
+    // item мы находим через catalog.js
+    add(item) {      
+        let find = this.items.find(el => item.productId == el.productId);
+
+        if(find) {
+            find.amount++
+        } else {
+            this.items.push(Object.assign({}, item, {amount: 1}));
+        }
+
+        this._render();
+    },
+    // 
+    _remove(id) {            
+        let find = this.items.find(el => el.productId == id);
+
+        if(find.amount > 1) {
+            find.amount--;
+        } else {
+            this.items.splice(this.items.indexOf(find), 1) // 1 - значит 1 элемент
+        }
+
+        this._render();
+    },
+    _handleEvents() {          
+        document.querySelector('#basket-btn').addEventListener('click', e => {
+        wrapper.classList.toggle('hidden')
+        // toggle убирает класс, а если есть то добовляет, вот и получается что при нажатиее он показывается, а при втором закрывается (hidden в _header.scss)
+        });
+        
+        //Удаление
+        container.addEventListener('click', event => {
+        if(event.target.name == 'remove') {
+            this._remove(event.target.dataset.id)
+        }
+    });
+    },
+
+    renderBasketTemplate(item, i) {
+        return `
+        <div class="cartFlex">
+            <div><img   src="${item.productImg}" alt="buy4"></div>
+    
+            <div class="textCenterCart">
+                <div class="textByCart">${item.productName}</div>
+            <div>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>                  
+            <div class="priceCart">
+                ${item.amount} x <span>${item.productPrice}</span> = ${item.amount * item.productPrice}
+            </div>
+            </div>
+            <div class="cartCircle">
+                <a href="#" class="far fa-times-circle faCart" name="remove" data-id="${item.productId}"></a>
+            </div>        
+        </div> 
+        <div class="horizontal cartHorizontal"></div>   
+    `
+    }
+}
+basket.init();
 
 
 
--- src/components/index/catalog.js
+++ src/components/index/catalog.js
@@ -1,75 +1,76 @@
-  const catalog = {
-      items: [],
-      container: null,
-      basket: null,
-      url: 'https://raw.githubusercontent.com/sergeykotenkogithub/imageProject/main/json/catalog.json',
-      init(basket) {
-          this.container = document.querySelector('#catalog');
-          // this.items = getCatalogItems(TITLES, PRICES);
-          this.basket = basket;
+// class Query в basket.js
 
-          //async
-          this._get(this.url) //Метод подключения к json на git
-          .then(catalog => {
-            this.items = catalog;
-            this._render();
-            this._handleEvents();
-          });
-      },
+let urlClassCatalog = new Query('https://raw.githubusercontent.com/sergeykotenkogithub/imageProject/main/json/catalog.json')
+let urlCatalog = urlClassCatalog.get()  
+let containerClassCatalog = new Query('#catalog')
+let containerCatalog = containerClassCatalog.init()
+// let initCatalogClass = new Query(urlCatalog)
+// let initCatalog = initCatalogClass.fdd()
 
-      // Метод подключения к json на git
-      _get(url) {
-        return fetch(url).then(d => d.json()) // сделает запрос за джейсоном, дождётся ответа и преобразует json в объект, который вернётся из даного метода
-      },
 
-      _render() {
-          let htmlStr = '';
+const catalog = {
+    items: [],
+    // container: null,
+    basket: null,
+    init(basket) {
+        this.basket = basket;
 
-          this.items.forEach((item, i) => {
-              htmlStr += this.renderCatalogTemplate(item, i);
-          });
-          this.container.innerHTML = htmlStr;
-      },
+        //async //Метод подключения к json на git
+        urlCatalog.then(catalog => {
+          this.items = catalog; // тут items был массивом (можно даже ему null задать, ничего не поменяется) и стал подтягивать массив из url
+          this._render();
+          this._handleEvents();
+        });
+    },
 
-      _handleEvents() {
-          this.container.addEventListener('click', event => {
-              if(event.target.name == 'add') {
-                  // console.log('КУПЛЕНО!')
-                  let id = event.target.dataset.id; //from data-id
-                  let item = this.items.find(el => el.productId == id);
+    _render() {
+        let htmlStr = '';
 
-                  // item = Object.assign({}, item, { productAmount: 1 });
-                  this.basket.add(item);
-                  // Находим товар, в basket.js мы добавляем
-              }
-          });
-      },
-     renderCatalogTemplate(item, i) {
-        return `
-        <div class="featuredBuyItem" id="catalog">
-        <!-- <a href="#"> Ссылка a, если надо, можно раскоментить -->
-          <div class="addToCart">
-            <div class="coverAddtoCart">
-              <div>
-                <!-- Задний фон картинки -->
-                <button class="backColor newBackColor"
-                              name="add"
-                              data-id="${item.productId}"
-                          >
-                          <img class="cartBuy" src="../src/assets/images/carsBuy.png" alt="cartBuy" />
-                          &nbsp ADD TO CART
-                          </button>          
-              </div>
+        this.items.forEach((item, i) => {
+            htmlStr += this.renderCatalogTemplate(item, i);
+        });
+        containerCatalog.innerHTML = htmlStr;
+    },
+
+    _handleEvents() {
+      containerCatalog.addEventListener('click', event => {
+            if(event.target.name == 'add') {
+                // console.log('КУПЛЕНО!')
+                let id = event.target.dataset.id; //from data-id
+                let item = this.items.find(el => el.productId == id);
+
+                // item = Object.assign({}, item, { productAmount: 1 });
+                this.basket.add(item);
+                // Находим товар, в basket.js мы добавляем
+            }
+        });
+    },
+    renderCatalogTemplate(item, i) {
+      return `
+      <div class="featuredBuyItem" id="catalog">
+      <!-- <a href="#"> Ссылка a, если надо, можно раскоментить -->
+        <div class="addToCart">
+          <div class="coverAddtoCart">
+            <div>
+              <!-- Задний фон картинки -->
+              <button class="backColor newBackColor"
+                            name="add"
+                            data-id="${item.productId}"
+                        >
+                        <img class="cartBuy" src="../src/assets/images/carsBuy.png" alt="cartBuy" />
+                        &nbsp ADD TO CART
+                        </button>          
             </div>
           </div>
-          <div><img src="${item.productImg}" /></div>
-        <!--  </a> -->
-        <div class="name_buy_item">${item.productName}</div>
-        <div class="price_item">$${item.productPrice}</div>
-      </div>
-        `
-      }
-  };
+        </div>
+        <div><img src="${item.productImg}" /></div>
+      <!--  </a> -->
+      <div class="name_buy_item">${item.productName}</div>
+      <div class="price_item">$${item.productPrice}</div>
+    </div>
+      `
+    }
+};
 
-  catalog.init(basket)
+catalog.init(basket)
 
